5cf116b8525ceb9e2352c5bcb06563073aa6ff85
DoaaAlsafadi <d.alsafadi.jo@gmail.com>
2017-09-23 17:54:54 +0300

    2nd commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express();
-var db = require('./database');
+var db = require('../database');
 
 app.use(express.static(__dirname + '/../client/dist'));
 
@@ -9,9 +9,15 @@ app.post('/repos/import', function (req, res) {
   // TODO
 });
 
-app.get('/repos', function (req, res) {
-  // TODO
+app.get('/repos/import', function(req, res){
+	res.sendFile(__dirname + '/import.html');
 });
+
+app.get('/repos', 
+function (req, res) {
+res.sendFile(__dirname + '/repos.html');
+});
+
 
 var port = 1128;
 
