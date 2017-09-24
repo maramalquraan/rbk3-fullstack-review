7516067ef8a61eddd834f6bb9e6d54fb40844a7a
DoaaAlsafadi <d.alsafadi.jo@gmail.com>
2017-09-24 20:44:33 +0300

    2nd day last commit

--- database/index.js
+++ database/index.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost/fetcher');
+var Schema=mongoose.Schema;
 
-var repoSchema = mongoose.Schema({
+var repoSchema = new Schema({
 	repoId: Number,
 	repoName: String,
 	userName: String
@@ -9,8 +10,18 @@ var repoSchema = mongoose.Schema({
 
 var Repo = mongoose.model('Repo', repoSchema);
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  console.log('Mongodb connection open');
+mongoose.connection.once('open', function(){
+	console.log('Connection has been made, now make fireworks..:D')
+}).on('error', function(error){
+	console.error("connection error;", error);
+});
+
+// db.on('error', function(err){
+//     console.error("connection error;", error);
+// });
+
+// db.once('open', function () {
+//   console.log('Mongodb connection open');
+// });
 
 module.exports = Repo;
